46e353d00dee4bceee32e90925a40d281fb40e66
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-12-24 23:38:25 -0300

    adicionando constantes cores

---
+++ constants/textos.py
@@ -0,0 +1,7 @@
+FONTE_TITULO = ("Segoe UI", 22, "bold")
+FONTE_SUBTITULO = ("Segoe UI", 17, "bold")
+
+FONTE_TEXTO = ("Segoe UI", 14)
+FONTE_PEQUENA = ("Segoe UI", 11)
+
+FONTE_BOTAO = ("Segoe UI", 14, "bold")
