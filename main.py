cf6e888d6aa5af6f707042f9eb70d7e999a4f489
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-12-24 23:39:57 -0300

    arquivo main temporário

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    pass
-
+from views.menu import TelaMenu
 
 if __name__ == "__main__":
-    main()
+    import customtkinter as ctk
+
+    class FakeController:
+
+        def alternar_modo_aparencia(self):
+            if ctk.get_appearance_mode() == "Light":
+                ctk.set_appearance_mode("Dark")
+            else:
+                ctk.set_appearance_mode("Light")
+
+    app = ctk.CTk()
+    app.title("Controle Operacional")
+    app.geometry("1280x720")
+    ctk.set_appearance_mode("Dark")
+
+    tela = TelaMenu(app, FakeController())
+    tela.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+    app.mainloop()
