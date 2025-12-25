55b156969749c6c1cb3eddbc1703ea58197f9f74
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-12-25 20:22:57 -0300

    nova pasta para controllers

---
+++ controllers/fake_controller.py
@@ -0,0 +1,17 @@
+import customtkinter as ctk
+
+class FakeController:
+    def __init__(self):
+        self.view = None
+
+    def set_view(self, view):
+        self.view = view
+
+    def alternar_modo_aparencia(self):
+        if ctk.get_appearance_mode() == "Light":
+            ctk.set_appearance_mode("Dark")
+        else:
+            ctk.set_appearance_mode("Light")
+
+    def mostrar_opcoes_escala(self):
+        self.view.mostrar_opcoes_escala()
