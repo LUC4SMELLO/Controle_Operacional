080b3eb2f770b3b3b11db5bc43d7bc0cb166adea
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-12-28 14:22:09 -0300

    feat: adicionando fonte para label

--- constants/textos.py
+++ constants/textos.py
@@ -1,5 +1,7 @@
 FONTE_TITULO = ("Segoe UI", 22, "bold")
-FONTE_SUBTITULO = ("Segoe UI", 17, "bold")
+FONTE_SUBTITULO = ("Segoe UI", 19, "bold")
+
+FONTE_LABEL = ("Segoe UI", 17, "bold")
 
 FONTE_TEXTO = ("Segoe UI", 14)
 FONTE_PEQUENA = ("Segoe UI", 11)
