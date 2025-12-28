0a4b02f122c0294a2e29237ec9eb34b96182c1dd
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-12-28 15:22:15 -0300

    feat: mudanças visuais

--- views/menu_view.py
+++ views/menu_view.py
@@ -7,6 +7,8 @@ from constants.textos import FONTE_BOTAO_SECUNDARIO
 
 from constants.cores import COR_LINHAS
 
+from constants.cores import (COR_BOTAO, HOVER_BOTAO, COR_TEXTO, COR_TEXTO_BOTAO)
+
 
 class TelaMenu(ctk.CTkFrame):
     def __init__(self, master, controller):
@@ -14,30 +16,93 @@ class TelaMenu(ctk.CTkFrame):
 
         self.controller = controller
 
-        ctk.CTkLabel(self, text="Menu", font=FONTE_TITULO).place(x=15, y=15)
+        ctk.CTkLabel(self, text="Menu", font=FONTE_TITULO, text_color=COR_TEXTO).place(x=15, y=15)
 
-        ctk.CTkFrame(self, width=5, height=1280, fg_color=COR_LINHAS).place(x=250, relx=0, relheight=1)
+        ctk.CTkFrame(self, width=5, height=1280, fg_color=COR_LINHAS).place(
+            x=250, relx=0, relheight=1
+        )
 
-        self.switch_alternar_modo = ctk.CTkSwitch(self, text="Tema", font=FONTE_SUBTITULO,command=self.controller.alternar_modo_aparencia)
+        self.switch_alternar_modo = ctk.CTkSwitch(
+            self,
+            text="Tema",
+            font=FONTE_SUBTITULO,
+            text_color=COR_TEXTO,
+            command=self.controller.alternar_modo_aparencia,
+        )
         self.switch_alternar_modo.place(x=120, y=20)
 
         # BOTÕES PRINCIPAIS
 
-        self.botao_escala = ctk.CTkButton(self, text="Escala", font=FONTE_BOTAO_PRINCIPAL, width=160, height=38, command=self.controller.mostrar_opcoes_escala)
+        self.botao_escala = ctk.CTkButton(
+            self,
+            text="Escala",
+            font=FONTE_BOTAO_PRINCIPAL,
+            width=160,
+            height=38,
+            command=self.controller.mostrar_opcoes_escala,
+            fg_color=COR_BOTAO,
+            hover_color=HOVER_BOTAO,
+            text_color=COR_TEXTO_BOTAO
+        )
         self.botao_escala.place(x=10, y=100)
 
-        self.botao_pendencia_troca = ctk.CTkButton(self, text="Pendência & Troca", font=FONTE_BOTAO_PRINCIPAL, width=160, height=38, command=self.controller.mostrar_opcoes_pendencia_troca)
+        self.botao_pendencia_troca = ctk.CTkButton(
+            self,
+            text="Pendência & Troca",
+            font=FONTE_BOTAO_PRINCIPAL,
+            width=160,
+            height=38,
+            command=self.controller.mostrar_opcoes_pendencia_troca,
+            fg_color= COR_BOTAO,
+            hover_color=HOVER_BOTAO,
+            text_color=COR_TEXTO_BOTAO
+        )
         self.botao_pendencia_troca.place(x=10, y=140)
-
 
         # BOTÕES SECUNDARIOS
 
-        self.botao_criar_escala = ctk.CTkButton(self, text="Editar", font=FONTE_BOTAO_SECUNDARIO)
-        self.botao_visualizar_escala =ctk.CTkButton(self, text="Visualizar", font=FONTE_BOTAO_SECUNDARIO)
+        self.botao_criar_escala = ctk.CTkButton(
+            self,
+            text="Editar",
+            font=FONTE_BOTAO_SECUNDARIO,
+            fg_color= COR_BOTAO,
+            hover_color=HOVER_BOTAO,
+            text_color=COR_TEXTO_BOTAO
+        )
+        self.botao_visualizar_escala = ctk.CTkButton(
+            self,
+            text="Visualizar",
+            font=FONTE_BOTAO_SECUNDARIO,
+            fg_color= COR_BOTAO,
+            hover_color=HOVER_BOTAO,
+            text_color=COR_TEXTO_BOTAO
+        )
 
-        self.botao_cadastrar_pendencia_troca = ctk.CTkButton(self, text="Cadastrar", font=FONTE_BOTAO_SECUNDARIO, command=self.controller.mostrar_tela_pendencia_troca)
-        self.botao_editar_pendencia_troca = ctk.CTkButton(self, text="Editar", font=FONTE_BOTAO_SECUNDARIO)
-        self.botao_excluir_pendencia_troca = ctk.CTkButton(self, text="Excluir", font=FONTE_BOTAO_SECUNDARIO)
+        self.botao_cadastrar_pendencia_troca = ctk.CTkButton(
+            self,
+            text="Cadastrar",
+            font=FONTE_BOTAO_SECUNDARIO,
+            command=self.controller.mostrar_tela_pendencia_troca,
+            fg_color= COR_BOTAO,
+            hover_color=HOVER_BOTAO,
+            text_color=COR_TEXTO_BOTAO
+        )
+        self.botao_editar_pendencia_troca = ctk.CTkButton(
+            self,
+            text="Editar",
+            font=FONTE_BOTAO_SECUNDARIO,
+            fg_color= COR_BOTAO,
+            hover_color=HOVER_BOTAO,
+            text_color=COR_TEXTO_BOTAO
+        )
+        self.botao_excluir_pendencia_troca = ctk.CTkButton(
+            self,
+            text="Excluir",
+            font=FONTE_BOTAO_SECUNDARIO,
+            fg_color= COR_BOTAO,
+            hover_color=HOVER_BOTAO,
+            text_color=COR_TEXTO_BOTAO
+        )
 
     def mostrar_opcoes_escala(self):
         self.botao_pendencia_troca.place(x=10, y=210)
@@ -56,7 +121,6 @@ class TelaMenu(ctk.CTkFrame):
         self.botao_criar_escala.place_forget()
         self.botao_visualizar_escala.place_forget()
 
-        
         self.botao_cadastrar_pendencia_troca.place(x=50, y=180)
         self.botao_editar_pendencia_troca.place(x=50, y=210)
         self.botao_excluir_pendencia_troca.place(x=50, y=240)
--- views/pendencia_troca_view.py
+++ views/pendencia_troca_view.py
@@ -12,6 +12,8 @@ from constants.textos import FONTE_BOTAO_SECUNDARIO
 
 from constants.cores import COR_LINHAS
 
+from constants.cores import (COR_BOTAO, HOVER_BOTAO, COR_TEXTO, COR_TEXTO_BOTAO)
+
 from constants.date_entry import (
     BACKGROUND,
     FOREGROUND,
@@ -34,13 +36,13 @@ class TelaPendenciaTroca(ctk.CTkFrame):
 
         self.controller = controller
 
-        ctk.CTkLabel(self, text="Pendência & Troca", font=FONTE_TITULO).place(x=40, y=15)
+        ctk.CTkLabel(self, text="Pendência & Troca", font=FONTE_TITULO, text_color=COR_TEXTO).place(x=40, y=15)
 
-        ctk.CTkLabel(self, text="Cadastrar", font=FONTE_SUBTITULO).place(x=40, y=65)
+        ctk.CTkLabel(self, text="Cadastrar", font=FONTE_SUBTITULO, text_color=COR_TEXTO).place(x=40, y=65)
 
         ctk.CTkFrame(self, width=500, height=2, fg_color=COR_LINHAS).place(x=40, y=105)
 
-        ctk.CTkLabel(self, text="Data:", font=FONTE_LABEL).place(x=135, y=130)
+        ctk.CTkLabel(self, text="Data:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=135, y=130)
 
         self.entry_data = DateEntry(
             self,
@@ -64,27 +66,27 @@ class TelaPendenciaTroca(ctk.CTkFrame):
             )
         self.entry_data.place(x=184, y=130)
         
-        ctk.CTkLabel(self, text="Carga:", font=FONTE_LABEL).place(x=127, y=165)
+        ctk.CTkLabel(self, text="Carga:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=127, y=165)
         self.entry_carga = ctk.CTkEntry(self, font=FONTE_TEXTO, width=100, height=30, corner_radius=2)
         self.entry_carga.place(x=184, y=165)
 
 
-        ctk.CTkLabel(self, text="Código Cliente:", font=FONTE_LABEL).place(x=55, y=200)
+        ctk.CTkLabel(self, text="Código Cliente:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=55, y=200)
         self.entry_codigo_cliente = ctk.CTkEntry(self, font=FONTE_TEXTO, width=100, height=30, corner_radius=2)
         self.entry_codigo_cliente.place(x=184, y=200)
 
 
-        ctk.CTkLabel(self, text="Tipo:", font=FONTE_LABEL).place(x=138, y=235)
+        ctk.CTkLabel(self, text="Tipo:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=138, y=235)
         self.entry_tipo = ctk.CTkComboBox(self, font=FONTE_TEXTO, values=["Pendência", "Troca"], width=150, height=30, corner_radius=2)
         self.entry_tipo.place(x=184, y=235)
 
-        ctk.CTkLabel(self, text="Responsável:", font=FONTE_LABEL).place(x=77, y=270)
+        ctk.CTkLabel(self, text="Responsável:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=77, y=270)
         self.entry_responsavel = ctk.CTkEntry(self, font=FONTE_TEXTO, width=150, height=30, corner_radius=2)
         self.entry_responsavel.place(x=184, y=270)
 
         ctk.CTkFrame(self, width=500, height=2, fg_color=COR_LINHAS).place(x=40, y=325)
 
-        ctk.CTkLabel(self, text="Código Produto:", font=FONTE_LABEL).place(x=45, y=350)
+        ctk.CTkLabel(self, text="Código Produto:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=45, y=350)
         self.entry_codigo_produto = ctk.CTkEntry(self, font=FONTE_TEXTO, width=100, height=30, corner_radius=2)
         self.entry_codigo_produto.place(x=184, y=350)
 
@@ -100,19 +102,38 @@ class TelaPendenciaTroca(ctk.CTkFrame):
             text="",
             width=20,
             height=20,
-            hover_color="#2A2A2A",
+            fg_color=COR_BOTAO,
+            hover_color=HOVER_BOTAO,
             cursor="hand2",
         )
         self.botao_buscar.place(x=295, y=350)
 
-        ctk.CTkLabel(self, text="Quantidade:", font=FONTE_LABEL).place(x=79, y=385)
+        ctk.CTkLabel(self, text="Quantidade:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=79, y=385)
         self.entry_quantidade = ctk.CTkEntry(self, font=FONTE_TEXTO, width=100, height=30, corner_radius=2)
         self.entry_quantidade.place(x=184, y=385)
 
         ctk.CTkFrame(self, width=500, height=2, fg_color=COR_LINHAS).place(x=40, y=440)
 
-        self.botao_confirmar = ctk.CTkButton(self, text="Confirmar", font=FONTE_BOTAO_SECUNDARIO, width=160, height=38)
+        self.botao_confirmar = ctk.CTkButton(
+            self,
+            text="Confirmar",
+            font=FONTE_BOTAO_PRINCIPAL,
+            width=160,
+            height=38,
+            fg_color=COR_BOTAO,
+            hover_color=HOVER_BOTAO,
+            text_color= COR_TEXTO_BOTAO,
+        )
         self.botao_confirmar.place(x=200, y=465)
 
-        self.botao_cancelar= ctk.CTkButton(self, text="Cancelar", font=FONTE_BOTAO_SECUNDARIO, width=160, height=38)
+        self.botao_cancelar= ctk.CTkButton(
+            self,
+            text="Cancelar",
+            font=FONTE_BOTAO_PRINCIPAL,
+            width=160,
+            height=38,
+            fg_color=COR_BOTAO,
+            hover_color=HOVER_BOTAO,
+            text_color=COR_TEXTO_BOTAO,
+        )
         self.botao_cancelar.place(x=382, y=465)
