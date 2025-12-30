c6f6348bffc86b29f6a55ca5d6d50c3dc8a7f3fb
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-12-30 14:45:18 -0300

    feat: nova função para cadastrar pendencia

--- controllers/pendencia/pendencia_controller.py
+++ controllers/pendencia/pendencia_controller.py
@@ -2,8 +2,9 @@ import customtkinter as ctk
 
 
 class PendenciaController:
-    def __init__(self):
+    def __init__(self, model):
         self.view = None
+        self.model = model
 
     def set_view(self, view):
         self.view = view
@@ -42,3 +43,18 @@ class PendenciaController:
     def focar_proximo(self, proximo_widget):
         proximo_widget.focus_set()
         return "break"
+    
+
+    def confirmar_cadastro_pendencia(self):
+
+        dados = {
+            "data": self.view.entry_data.get(),
+            "carga": self.view.entry_carga.get(),
+            "codigo_cliente": self.view.entry_codigo_cliente.get(),
+            "tipo": self.view.entry_tipo.get(),
+            "responsavel": self.view.entry_responsavel.get(),
+            "codigo_produto": self.view.entry_codigo_produto.get(),
+            "quantidade": self.view.entry_quantidade.get()
+        }
+
+        self.model.cadastrar_pendencia(dados)
