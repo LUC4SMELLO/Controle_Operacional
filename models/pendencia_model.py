612f07aebbf6bc77754ee0bb39a9443ad9f871e5
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-12-31 15:42:29 -0300

    feat: funções para buscar e atualizar no banco de dados

--- models/pendencia_model.py
+++ models/pendencia_model.py
@@ -37,3 +37,63 @@ class PendenciaModel:
         
         conexao.commit()
         conexao.close()
+
+    def editar_pendencia(self, dados):
+
+        conexao = conectar_banco_de_dados_pendencias()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+            f"""
+            UPDATE {TABELA_PENDENCIAS}
+            SET data = ?,
+            carga = ?,
+            codigo_cliente = ?,
+            tipo = ?,
+            responsavel = ?,
+            codigo_produto = ?,
+            quantidade = ?
+            WHERE cupom = ?
+            """,
+                (
+                    dados["data"],
+                    dados["carga"],
+                    dados["codigo_cliente"],
+                    dados["tipo"],
+                    dados["responsavel"],
+                    dados["codigo_produto"],
+                    dados["quantidade"],
+                    dados["cupom"]
+                )
+            )
+
+        conexao.commit()
+        conexao.close()
+
+
+    def buscar_pendencia(self, cupom):
+
+        conexao = conectar_banco_de_dados_pendencias()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+        f"""
+        SELECT
+        cupom,
+        data,
+        carga,
+        codigo_cliente,
+        tipo,
+        responsavel,
+        codigo_produto,
+        quantidade
+        FROM {TABELA_PENDENCIAS}
+        WHERE cupom = ?
+        """, (cupom,)
+        )
+
+        resultado = cursor.fetchone()
+
+        conexao.close()
+
+        return resultado
