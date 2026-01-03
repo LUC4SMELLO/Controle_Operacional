48a747a44a1465cc62818de8e11adc3888a83733
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-01-02 22:29:14 -0300

    feat: adicionando treeview tela relatório pendências

--- views/relatorios/relatorio_pendencia_view.py
+++ views/relatorios/relatorio_pendencia_view.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from tkinter import ttk
 from CTkMessagebox import CTkMessagebox
 from PIL import Image
 from tkcalendar import DateEntry
@@ -8,6 +9,7 @@ from constants.textos import (
     FONTE_TITULO,
     FONTE_SUBTITULO,
     FONTE_LABEL,
+    FONTE_TREE_VIEW,
     FONTE_TEXTO,
     FONTE_PEQUENA,
     FONTE_BOTAO_PRINCIPAL,
@@ -76,7 +78,7 @@ class RelatorioPendenciaView(ctk.CTkFrame):
             borderwidth = BORDERWIDTH,
             selectmode = 'day',
             date_pattern = 'dd/mm/yyyy',
-            width=10
+            width=11
             )
         self.entry_data.place(x=346, y=130)
 
@@ -120,3 +122,54 @@ class RelatorioPendenciaView(ctk.CTkFrame):
         self.botao_buscar_pendencia.place(x=801, y=180)
 
         ctk.CTkFrame(self, width=950, height=2, fg_color=COR_LINHAS).place(x=40, y=270)
+
+
+
+
+
+
+        style = ttk.Style()
+        style.theme_use("default")
+
+        style.configure(
+            "Treeview.Heading",
+            font=FONTE_TREE_VIEW,
+            background="#343638",
+            foreground="#FFFFFF",
+        )
+        
+
+        colunas = ("cupom", "data", "carga", "codigo_cliente", "tipo", "responsavel", "codigo_produto", "quantidade")
+        self.tree = ttk.Treeview(
+            self,
+            columns=colunas,
+            show="headings",
+            height=12
+            )
+        self.tree.place(x=40, y=310, width=935, height=350)
+
+        scroll_x = ttk.Scrollbar(self, orient="horizontal", command=self.tree.xview)
+        scroll_y = ttk.Scrollbar(self, orient="vertical", command=self.tree.yview)
+
+        scroll_x.place(x=40, y=660, width=935)
+        scroll_y.place(x=975, y=310, height=350)
+
+        self.tree.configure(
+            xscrollcommand=scroll_x.set,
+            yscrollcommand=scroll_y.set
+        )
+
+
+        self.tree.heading("cupom", text="Cupom", anchor="center")
+        self.tree.heading("data", text="Data", anchor="center")
+        self.tree.heading("carga", text="Carga", anchor="center")
+        self.tree.heading("codigo_cliente", text="Código Cliente", anchor="center")
+        self.tree.heading("tipo", text="Tipo", anchor="center")
+        self.tree.heading("responsavel", text="Responsável", anchor="center")
+        self.tree.heading("codigo_produto", text="Código Produto", anchor="center")
+        self.tree.heading("quantidade", text="Quantidade", anchor="center")
+
+
+        self.tree.bind("<MouseWheel>", lambda e: self.tree.yview_scroll(-int(e.delta / 2.5), "units"))
+        self.tree.bind("<Shift-MouseWheel>", lambda e: self.tree.xview_scroll(-int(e.delta / 2.5), "units"))
+
