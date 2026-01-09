fb849cbc7d573f07705c4f6eb0eab36ac8718531
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-01-09 14:43:03 -0300

    feat: mudando o layout da tela relatório pendências

--- views/relatorios/relatorio_pendencia_view.py
+++ views/relatorios/relatorio_pendencia_view.py
@@ -54,13 +54,11 @@ class RelatorioPendenciaView(ctk.CTkFrame):
         ctk.CTkFrame(self, width=950, height=2, fg_color=COR_LINHAS).place(x=40, y=105)
 
 
-        ctk.CTkLabel(self, text="Cupom:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=110, y=130)
-        self.entry_cupom = ctk.CTkEntry(self, font=FONTE_TEXTO, width=62, height=30, corner_radius=2)
-        self.entry_cupom.place(x=177, y=130)
 
-        ctk.CTkLabel(self, text="Data:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=296, y=130)
 
-        self.entry_data = DateEntry(
+        ctk.CTkLabel(self, text="Período:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=40, y=130)
+
+        self.entry_data_inicio = DateEntry(
             self,
             justify = "center", 
             font = FONTE_PEQUENA,
@@ -80,35 +78,55 @@ class RelatorioPendenciaView(ctk.CTkFrame):
             date_pattern = 'dd/mm/yyyy',
             width=11
             )
-        self.entry_data.delete(0, ctk.END)
-        self.entry_data.place(x=346, y=130)
+        self.entry_data_inicio.delete(0, ctk.END)
+        self.entry_data_inicio.place(x=115, y=130)
 
-        ctk.CTkLabel(self, text="Carga:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=515, y=130)
+        ctk.CTkLabel(self, text="a", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=230, y=130)
+
+        self.entry_data_fim = DateEntry(
+            self,
+            justify = "center", 
+            font = FONTE_PEQUENA,
+            background = BACKGROUND,
+            foreground = FOREGROUND,       
+            headersbackground = HEADERSBACKGROUND, 
+            headersforeground = HEADERSFOREGROUND, 
+            normalbackground = NORMALBACKGROUND, 
+            normalforeground = NORMALFOREGROUND, 
+            weekendbackground = WEEKENDBACKGROUND,
+            weekendforeground = WEEKENDFOREGROUND,
+            selectbackground = SELECTBACKGROUND, 
+            selectforeground = SELECTFOREGROUND, 
+            bordercolor = BORDERCOLOR,      
+            borderwidth = BORDERWIDTH,
+            selectmode = 'day',
+            date_pattern = 'dd/mm/yyyy',
+            width=11
+            )
+        self.entry_data_fim.delete(0, ctk.END)
+        self.entry_data_fim.place(x=250, y=130)
+
+        ctk.CTkLabel(self, text="Cupom:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=370, y=130)
+        self.entry_cupom = ctk.CTkEntry(self, font=FONTE_TEXTO, width=62, height=30, corner_radius=2)
+        self.entry_cupom.place(x=437, y=130)
+
+        ctk.CTkLabel(self, text="Carga:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=510, y=130)
         self.entry_carga = ctk.CTkEntry(self, font=FONTE_TEXTO, width=70, height=30, corner_radius=2)
-        self.entry_carga.place(x=570, y=130)
+        self.entry_carga.place(x=565, y=130)
 
-        ctk.CTkLabel(self, text="Tipo:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=755, y=130)
-        self.entry_tipo = ctk.CTkComboBox(self, font=FONTE_TEXTO, values=["Ambos", "Pendência", "Troca"], width=110, height=30, corner_radius=2)
+        ctk.CTkLabel(self, text="Tipo:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=650, y=130)
+        self.entry_tipo = ctk.CTkComboBox(self, font=FONTE_TEXTO, justify="center", values=["Ambos", "Pendência", "Troca"], width=110, height=30, corner_radius=2)
         self.entry_tipo.set("Ambos")
-        self.entry_tipo.place(x=801, y=130)
+        self.entry_tipo.place(x=696, y=130)
 
-
-
-        ctk.CTkLabel(self, text="Código Produto:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=40, y=180)
-        self.entry_codigo_produto = ctk.CTkEntry(self, font=FONTE_TEXTO, width=62, height=30, corner_radius=2)
-        self.entry_codigo_produto.place(x=177, y=180)
-
-        ctk.CTkLabel(self, text="Quantidade:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=75, y=230)
-        self.entry_quantidade = ctk.CTkEntry(self, font=FONTE_TEXTO, width=62, height=30, corner_radius=2)
-        self.entry_quantidade.place(x=177, y=230)
-
-        ctk.CTkLabel(self, text="Código Cliente:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=256, y=180)
+        ctk.CTkLabel(self, text="Código Cliente:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=40, y=180)
         self.entry_codigo_cliente = ctk.CTkEntry(self, font=FONTE_TEXTO, width=65, height=30, corner_radius=2)
-        self.entry_codigo_cliente.place(x=385, y=180)
+        self.entry_codigo_cliente.place(x=170, y=180)
 
-        ctk.CTkLabel(self, text="Responsável:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=465, y=180)
-        self.entry_responsavel = ctk.CTkEntry(self, font=FONTE_TEXTO, width=150, height=30, corner_radius=2)
-        self.entry_responsavel.place(x=570, y=180)
+        ctk.CTkLabel(self, text="Código Produto:", font=FONTE_LABEL, text_color=COR_TEXTO).place(x=250, y=180)
+        self.entry_codigo_produto = ctk.CTkEntry(self, font=FONTE_TEXTO, width=62, height=30, corner_radius=2)
+        self.entry_codigo_produto.place(x=390, y=180)
+
 
         self.botao_buscar_pendencia = ctk.CTkButton(
             self,
@@ -121,9 +139,9 @@ class RelatorioPendenciaView(ctk.CTkFrame):
             hover_color=HOVER_BOTAO,
             text_color=COR_TEXTO_BOTAO
         )
-        self.botao_buscar_pendencia.place(x=801, y=180)
+        self.botao_buscar_pendencia.place(x=880, y=130)
 
-        ctk.CTkFrame(self, width=950, height=2, fg_color=COR_LINHAS).place(x=40, y=270)
+        ctk.CTkFrame(self, width=950, height=2, fg_color=COR_LINHAS).place(x=40, y=240)
 
 
 
