28a31f253590aa11704faf0f571f07100588daf5
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-01-21 14:14:21 -0300

    feat: banco de dados para os caminhões

---
+++ database/banco_dados_veiculos.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+from constants.banco_dados import BANCO_DADOS_VEICULOS, TABELA_VEICULOS
+
+
+def conectar_banco_de_dados_veiculos():
+    return sqlite3.connect(BANCO_DADOS_VEICULOS)
+
+
+def criar_tabela_veiculos():
+    
+    conexao = conectar_banco_de_dados_veiculos()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+    f"""
+    CREATE TABLE IF NOT EXISTS {TABELA_VEICULOS} (
+    codigo INTEGER PRIMARY KEY,
+    placa VARCHAR(10),
+    codigo_motorista VARCHAR(5)
+    )
+    """
+    )
+
+    conexao.commit()
+    conexao.close()
