0b176ab7eb1c84feb08e1fd087724fb8cfad9e14
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-01-22 16:24:29 -0300

    feat: janela agora inicia em tela cheia

--- views/janela.py
+++ views/janela.py
@@ -4,7 +4,7 @@ import customtkinter as ctk
 janela = ctk.CTk()
 janela.title("Controle Operacional")
 janela.geometry("1280x720")
-janela.state("zoomed")
+janela.after(0, lambda: janela.state("zoomed"))
 
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("dark-blue")
