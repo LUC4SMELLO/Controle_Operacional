36636ad83482d6839ecb02fd496753c533794482
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-02-01 11:07:22 -0300

    feat: model veículo

---
+++ models/veiculo/veiculo_model.py
@@ -0,0 +1,6 @@
+from database.banco_dados_veiculos import conectar_banco_de_dados_veiculos
+from constants.banco_dados import TABELA_VEICULOS
+
+class VeiculoModel:
+    def __init__(self):
+        pass
