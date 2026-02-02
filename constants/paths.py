109a092314aafc5ce0663b2779872e6b89123044
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-02-02 14:10:26 -0300

    feat: caminho absoluto para as imagens

--- constants/paths.py
+++ constants/paths.py
@@ -4,6 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ASSETS_DIR = BASE_DIR / "assets"
 ICONS_DIR = ASSETS_DIR / "icons"
+IMAGES_DIR = ASSETS_DIR / "images"
 
 ARCHIVES_DIR = BASE_DIR / "archives"
 
